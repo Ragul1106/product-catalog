b1965bc3e38f02f0231fba83ffac3b5df241974e
Ragul116 <ragulranjith1106@gmail.com>
2025-08-14 13:56:27 +0530

    initial commit

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv(
         "DATABASE_URL",
-        "mysql+pymysql://root:Ragul%40116@localhost/product_db"
+        "sqlite:///product_db.sqlite"  
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY", "mysecretkey")
